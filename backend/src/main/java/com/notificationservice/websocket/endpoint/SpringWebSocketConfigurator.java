6011834818c101c864e8481b551b6bf673251377
Wilsonf8 <Wilsonflores880@gmail.com>
2026-01-26 22:22:30 -0600

    ws logic

---
+++ backend/src/main/java/com/notificationservice/websocket/endpoint/SpringWebSocketConfigurator.java
@@ -0,0 +1,31 @@
+package com.notificationservice.websocket.endpoint;
+
+import jakarta.websocket.server.ServerEndpointConfig;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Configurator that allows JSR 356 WebSocket endpoints to be managed by Spring.
+ * This enables dependency injection in WebSocket endpoint classes.
+ */
+@Component
+public class SpringWebSocketConfigurator extends ServerEndpointConfig.Configurator implements ApplicationContextAware {
+
+    private static volatile BeanFactory context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringWebSocketConfigurator.context = applicationContext;
+    }
+
+    @Override
+    public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
+        if (context == null) {
+            throw new InstantiationException("Spring context not available");
+        }
+        return context.getBean(endpointClass);
+    }
+}
